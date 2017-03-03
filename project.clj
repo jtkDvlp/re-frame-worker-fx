4bdc5c515f22aeed3b4c335c8316d6c58faadb4e
JT Knabenschuh <jtkdevelopments@gmail.com>
2017-03-04 00:03:10 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame-worker-fx "1.0.0-alpha"
+(defproject re-frame-worker-fx "1.0.1-alpha"
   :description "A re-frame effects handler for performing async tasks via cljs-workers"
   :url "https://github.com/jtkDvlp/re-frame-worker-fx"
   :license {:name "MIT"}
